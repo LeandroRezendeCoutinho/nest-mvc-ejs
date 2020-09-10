95c9967283637a6396a9768e1d0a04eccc802324
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2020-09-10 12:14:55 -0300

    add cluster possibility

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,25 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 
+// import * as cluster from 'cluster';
+// import * as os from 'os';
+// import { Logger } from '@nestjs/common';
+
+// if (cluster.isMaster) {
+//   const cpuCount = os.cpus().length;
+
+//   for (let i = 0; i < cpuCount; i += 1) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('online', worker => {
+//     Logger.log('Worker ' + worker.process.pid + ' is online.');
+//   });
+//   cluster.on('exit', ({ process }, code, signal) => {
+//     Logger.log('worker ' + process.pid + ' died.');
+//   });
+// } else {
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
@@ -15,3 +34,4 @@ async function bootstrap() {
   await app.listen(3000);
 }
 bootstrap();
+// }
